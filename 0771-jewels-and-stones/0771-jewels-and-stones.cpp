70317399f7633094c3cd94f4edd0be61f0c6e08c
akshat27D <91897122+akshat27D@users.noreply.github.com>
2022-11-19 19:49:32 +0530

    Time: 6 ms (25.90%), Space: 6.3 MB (40.40%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int numJewelsInStones(string str, string atr) {
-        int count=0;
-        int  n = str.size();
-        int m = atr.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(str[i]==atr[j]){
-                    count++;
-                }
-            }
-            
-        }
-        return count;
+    int numJewelsInStones(string J, string S) {
+        int res = 0;
+        unordered_set<char> setJ(J.begin(), J.end());
+        for (char s : S)
+            if (setJ.count(s)) res++;
+        return res;
         
         
     }
