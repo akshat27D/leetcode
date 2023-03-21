5e52bcf79810ba37c9660518d998d06f6cb7bd2a
akshat27D <91897122+akshat27D@users.noreply.github.com>
2023-03-21 10:58:55 +0530

    Time: 17 ms (36.63%), Space: 21.8 MB (56.07%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,28 +11,19 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode* root,long low ,long high){
-        // Empty trees are valid BSTs.
-        if(root == NULL){
+    bool func(TreeNode* root , long low , long high){
+        if(root==NULL){
             return true;
         }
-        
-        // Condition of Binary Search Tree 
-        // left side node values of the root should be less value than the root and right side should be greater
-        // If that is true check recursivley for the root child's
-        
-        if((root->val < high) && (root->val > low)){
-          return (helper(root->left, low, root->val) && helper(root->right, root->val, high));
-
+        if((root->val<high) && (root->val > low)){
+            return ((func(root->left,low,root->val)) && (func(root->right,root->val,high)));
         }else{
-            //if this root node violates the min/max constraint
             return false;
         }
-        
     }
-    
     bool isValidBST(TreeNode* root) {
-        bool ans = helper(root, LONG_MIN, LONG_MAX);
+        bool ans = func(root,LONG_MIN,LONG_MAX);
         return ans;
+        
     }
 };
