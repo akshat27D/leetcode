c48e196af62025287628cde55cebdd5fbe355ac3
akshat27D <91897122+akshat27D@users.noreply.github.com>
2023-04-06 15:31:13 +0530

    Time: 3 ms (96.65%), Space: 42.2 MB (57.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -5,19 +5,33 @@ class Solution {
         
     }
     public static int lis(int[] arr){
-        int[] dp  = new int[arr.length];
-        Arrays.fill(dp,1);
-        for(int i=0;i<dp.length;i++){
-            for(int j = i-1;j>=0;j--){
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
+        int dp[] = new int[arr.length];
+        dp[0] = arr[0];
+        int len =1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>dp[len-1]){
+                dp[len] = arr[i];
+                len++;
+            }else{
+                int ith = Search(dp,0,len-1,arr[i]);
+                dp[ith]=arr[i];
+                
             }
         }
-        int max = dp[0];
-        for(int i=0;i<dp.length;i++){
-            max = Math.max(max,dp[i]);
+        return len;
+    }
+    private static int Search(int dp[] , int si , int ei ,int item){
+        int ans =0;
+        while(si<=ei){
+            int mid = (si+ei)/2;
+            if(dp[mid]>=item){
+                ans =mid;
+                ei = mid-1;
+            }else{
+                si=mid+1;
+                
+            }
         }
-        return max;
+        return ans;
     }
 }
